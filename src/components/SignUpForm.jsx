91ab0e607f57df5f8d072db5de9dbb5d9cb34f4b
JThomas62 <jacobt257@gmail.com>
2023-09-14 21:41:31 -0400

    Sign Up Has  Functionality

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,30 +1,46 @@
 import { useState } from "react";
 
-
 export default function SignUpForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("null");
-  
+  const [error, setError] = useState(null);
+
   async function handleSubmit(event) {
     event.preventDefault();
-    console.log("Hello");
+
+    try {
+      const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Server Error");
+      }
+
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      setError(error.message);
+    }
   }
 
-  
-
   return (
-  <>
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username: <input value={username} onChange={(e) => setUsername(e.target.value)}/>
-      </label>
-      <label>
-        Password: <input value={password} onChange={(e) => setPassword(e.target.value)}/>
-      </label>
-      <button>Submit</button>
-    </form>
-  </>
-    
-  )
+    <>
+      <h2>Sign Up</h2>
+        {error && <p>{error}</p>}
+          <form onSubmit={handleSubmit}>
+            <label>
+              Username: <input value={username} onChange={(e) => setUsername(e.target.value)}/>
+            </label>
+            <label>
+              Password: <input value={password} onChange={(e) => setPassword(e.target.value)}/>
+            </label>
+            <button>Submit</button>
+          </form>
+    </>
+  );
 }
