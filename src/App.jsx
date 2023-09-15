3b7c7d313892853163c829de226e05642a49360e
JThomas62 <jacobt257@gmail.com>
2023-09-14 21:12:16 -0400

    Form Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
-import './App.css'
+import './App.css';
+import Authenitcate from './components/Authenitcate';
+import SignUpForm from './components/SignUpForm';
+
+export default function App() {
+  
+  return (
+    <>
+      <Authenitcate />
+      <SignUpForm />
+    </>
+  );
+}
 
 
