764301b7ea9997f3ccb33388a3533a3827dbf053
JThomas62 <jacobt257@gmail.com>
2023-09-15 21:05:09 -0400

    Added Comments For Better Understanding What Is Going On

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import { useState } from 'react';
 import Authenitcate from './components/Authenitcate';
 import SignUpForm from './components/SignUpForm';
 
+// Defining the main App component
 export default function App() {
-
+  // Using the useState hook to manage the 'token' state
   const [token, setToken] = useState(null);
-  
+
+  // Rendering the Authenticate and SignUpForm components with 'token' and 'setToken' props
   return (
     <>
-      <Authenitcate token={token} setToken={setToken}/>
-      <SignUpForm token={token} setToken={setToken}/>
+      {/* Rendering the Authenticate component and passing 'token' and 'setToken' props */}
+      <Authenitcate token={token} setToken={setToken} />
+      
+      {/* Rendering the SignUpForm component and passing 'token' and 'setToken' props */}
+      <SignUpForm token={token} setToken={setToken} />
     </>
   );
 }
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 
+// Defining the SignUpForm component
 export default function SignUpForm({ setToken }) {
+  // Using the useState hook to manage the 'username', 'password', and 'error' states
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
 
+  // Handling form submission
   async function handleSubmit(event) {
     event.preventDefault();
 
     try {
+      // Sending a POST request to the signup API endpoint
       const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
         method: "POST",
         headers: {
@@ -17,29 +21,32 @@ export default function SignUpForm({ setToken }) {
         body: JSON.stringify({ username, password }),
       });
 
+      // Parsing the JSON response
       const result = await response.json();
       console.log(result);
-      
-      setToken(result.token);
 
+      // Setting the token in the parent component's state (provided through props)
+      setToken(result.token);
     } catch (error) {
+      // Handling and displaying any errors that occur during form submission
       setError(error.message);
     }
   }
 
+  // Rendering the sign-up form
   return (
     <>
       <h2>Sign Up</h2>
-        {error && <p>{error}</p>}
-          <form onSubmit={handleSubmit}>
-            <label className="username">
-              Username: <input className="username-input" value={username} onChange={(e) => setUsername(e.target.value)}/>
-            </label>
-            <label className="password">
-              Password: <input className="password-input" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-            </label>
-            <button className="submit-button">Submit</button>
-          </form>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label className="username">
+          Username: <input className="username-input" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <label className="password">
+          Password: <input className="password-input" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <button className="submit-button">Submit</button>
+      </form>
     </>
   );
 }
